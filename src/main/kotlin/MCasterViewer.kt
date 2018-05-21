a0b438dd7d92f5f5078475cbdbabce0ecdccfc39
Alexey Subbotin <alexey.subbotin@tinypass.com>
2018-05-20 21:12:26 -0400

    [viewer corrected to work similar to player]

--- src/main/kotlin/MCasterViewer.kt
+++ src/main/kotlin/MCasterViewer.kt
@@ -21,21 +21,44 @@ fun main(args: Array<String>) {
                 staffView[it] = ArrayList((0..staff.measures.size).map { CharArray(0) })
             }
 
+    val extFactor = 2
     staff.measures.forEachIndexed { idx, measure ->
-        val numberOfTicks = measure.tickFraction.numerator
+        println("Measure: ${measure.tickFraction.numerator}/${measure.tickFraction.denominator}")
+        padTypes.forEach {
+            if (staffView[it.abbreviation]!![idx].isEmpty()) {
+                staffView[it.abbreviation]!![idx] = "-".repeat(measure.tickFraction.numerator * extFactor).toCharArray()
+            }
+        }
+        (0..(measure.tickFraction.numerator - 1)).forEach { pos ->
+            val tickFraction = SimpleFraction(pos, measure.tickFraction.denominator)
+            println(">> tick: $tickFraction")
+            measure.notes.entries.asSequence()
+                    .filter { it.value.any { it == tickFraction } }
+                    .map {
+                        println("\t${it.key}: ${it.value.first { it == tickFraction }}")
+                        it.key
+                    }
+                    .forEach {
+                        val i = tickFraction.numerator * extFactor
+                        staffView[it.abbreviation]!![idx][i] = it.note
+                    }
 
-        padTypes
-                .map { it to (measure.notes[it] ?: emptyList()) }
-                .forEach { note ->
-                    val extFactor = 3
-                    if (staffView[note.first.abbreviation]!![idx].isEmpty()) {
-                        staffView[note.first.abbreviation]!![idx] = "-".repeat(numberOfTicks * extFactor).toCharArray()
-                    }
-                    note.second.forEach {
-                        val pos = it.numerator * numberOfTicks / it.denominator * extFactor
-                        staffView[note.first.abbreviation]!![idx][pos] = note.first.note
-                    }
-                }
+        }
+        println(">>>\n")
+
+
+//        padTypes
+//                .map { it to (measure.notes[it] ?: emptyList()) }
+//                .forEach { note ->
+//                    val extFactor = 2
+//                    if (staffView[note.first.abbreviation]!![idx].isEmpty()) {
+//                        staffView[note.first.abbreviation]!![idx] = "-".repeat(numberOfTicks.numerator * extFactor).toCharArray()
+//                    }
+//                    note.second.forEach {
+//                        val pos = (it.numerator * numberOfTicks.numerator) / (it.denominator * numberOfTicks.denominator) * extFactor
+//                        staffView[note.first.abbreviation]!![idx][pos] = note.first.note
+//                    }
+//                }
     }
     staffView.entries.forEach {
         print(it.key + "||")
